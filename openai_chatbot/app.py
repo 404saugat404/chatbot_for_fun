bfbd1eaef305a368194a932856de34ac3a1f1607
saugat <saugat556513@gmail.com>
2024-07-26 12:33:32 +0545

    chatbot with frontend ready

--- openai_chatbot/app.py
+++ openai_chatbot/app.py
@@ -1,4 +1,4 @@
-from langchain.llms import openai
+from langchain_openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -6,7 +6,18 @@ import os
 import streamlit as st
 
 api_key = os.getenv('open_ai_api')
-print(api_key)
+
 
 def open_ai_responst(question):
-    
+    llm=OpenAI(openai_api_key=api_key,model_name='gpt-3.5-turbo-instruct',temperature=0.6)
+    response=llm(question)
+    return response
+
+st.set_page_config("Q&A")
+st.header('langchain application')
+input=st.text_input("input",key="input")
+response=open_ai_responst(input)
+submit_button=st.button('ask the question')
+if submit_button:
+    st.subheader('response is :')
+    st.write(response)
